4bd8bb6f24e99f94e9b3f5058ff6e88f92c6cfde
Jason Williscroft <me@karmanivero.us>
2023-05-09 16:58:24 +0800

    bugfix

--- bin/getdotenv/index.js
+++ bin/getdotenv/index.js
@@ -139,7 +139,10 @@ if (command || program.args.length) {
     const { error, status } = spawn.sync(argv[0], argv.slice(1), {
       stdio: 'inherit',
     });
-    if ((error || status) && throwError)
-      throw error ?? new Error(`exited with code ${status}`);
+
+    if (throwError) {
+      if (error) throw error;
+      if (status) throw new Error(`exited with code ${status}`);
+    }
   }
 }
