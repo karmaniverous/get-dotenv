feee7f8d5482bd607cfa6f1e6f935f5fa1766c2a
Jason Williscroft <me@karmanivero.us>
2026-01-08 13:37:27 +0800

    added missing exports

--- src/index.ts
+++ src/index.ts
@@ -85,6 +85,12 @@ export type {
   DotenvProvenanceOp,
   DotenvVarsProvenanceEntry,
 } from './env';
-export { applyIncludeExclude, interpolateDeep } from './util';
+export {
+  applyIncludeExclude,
+  interpolateDeep,
+  parseFiniteNumber,
+  parseNonNegativeInt,
+  parsePositiveInt,
+} from './util';
 /** @deprecated Import \{ z \} from 'zod' directly. Will be removed in v7. */
 export { z } from 'zod';
