092df6baad8774515fd163479daeba120c26a18e
jgs-jeeves <jeeves@johngalt.id>
2026-06-04 03:35:44 +0000

    chore: remove temp commit helper scripts

--- tmp-commit.js
+++
@@ -1,5 +0,0 @@
-const { execSync } = require('child_process');
-const cwd = 'D:/repos/karmaniverous/get-dotenv';
-const run = (c) => console.log(execSync(c, { cwd, encoding: 'utf8' }));
-run('git commit -m "docs(aws): fix afterResolve contract placement and add inline rationale"');
-run('git push origin bugfix/login-on-demand-scope');
